2223244fd1e422d16e9cbee013f61a6b5f6e7b0a
Larissa Dias <lmdias@uw.edu>
2025-04-15 10:54:52 -0700

    adding first three items from the package

---
+++ adjust_pH_DIC.py
@@ -0,0 +1,40 @@
+def adjust_pH_DIC(DesiredVariables, VerboseTF, Dates, Est_pre={}, PredictorMeasurements={}, OutputCoordinates={}, **kwargs):
+
+    """
+    Adjusting pH and DIC for anthropogenic carbon.
+    """
+
+    import numpy as np
+    import math    
+    from SimpleCantEstimateLR import SimpleCantEstimateLR
+    
+    YouHaveBeenWarnedCanth = False
+
+    Cant_adjusted={}
+    combos2 = list(Est_pre.keys())
+    values2 = list(Est_pre.values())
+    values2 = np.array(values2).T
+    if "EstDates" in kwargs and ("DIC" in DesiredVariables or "pH" in DesiredVariables):      
+        if not YouHaveBeenWarnedCanth:
+            if VerboseTF:
+                print("Estimating anthropogenic carbon for PyESPER_NN.")
+            longitude = np.mod(OutputCoordinates["longitude"], 360)
+            latitude = np.array(OutputCoordinates["latitude"])
+            depth = np.array(OutputCoordinates["depth"])
+            Cant, Cant2002 = SimpleCantEstimateLR(Dates, longitude, latitude, depth)
+            YouHaveBeenWarnedCanth = True
+       
+        for combo, a in zip(combos2, values2):
+            dic = []
+            if combo.startswith("DIC"):
+
+                for vala, Canta, Cant2002a in zip(a, Cant, Cant2002):
+                    if math.isnan(vala): 
+                        dic.append("nan")
+                    else:
+                        dic.append(vala + Canta - Cant2002a)
+            else:
+                dic = list(a)
+            Cant_adjusted[combo] = dic
+
+    return Cant_adjusted
